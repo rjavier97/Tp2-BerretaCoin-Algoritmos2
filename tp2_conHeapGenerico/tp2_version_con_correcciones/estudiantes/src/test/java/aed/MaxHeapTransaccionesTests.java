4c5e39fa2529dbc4cc86d43ee60746c2038c67be
lu0bln <159673640+lu0bln@users.noreply.github.com>
2025-07-13 22:20:22 -0300

    Update MaxHeapTransaccionesTests.java

--- tp2_conHeapGenerico/tp2_version_con_correcciones/estudiantes/src/test/java/aed/MaxHeapTransaccionesTests.java
+++ tp2_conHeapGenerico/tp2_version_con_correcciones/estudiantes/src/test/java/aed/MaxHeapTransaccionesTests.java
@@ -10,31 +10,55 @@ import aed.Clases.MaxHeapTransacciones;
 
 public class MaxHeapTransaccionesTests {
     
-Transaccion una = new Transaccion (0, 0, 2, 10);
-Transaccion dos = new Transaccion (1, 2, 3, 4);
-Transaccion tres = new Transaccion (2, 3, 4, 6);
-Transaccion cuatro = new Transaccion (3, 4, 5, 15);
-Transaccion cinco = new Transaccion (4, 7, 6, 64);
-Transaccion seis = new Transaccion (5, 8, 9, 2);
-Transaccion siete = new Transaccion (6, 7, 1, 43);
-Transaccion ocho = new Transaccion (7, 5, 2, 23);
+Transaccion tx_una = new Transaccion (0, 0, 2, 10);
+Transaccion tx_dos = new Transaccion (1, 2, 3, 4);
+Transaccion tx_tres = new Transaccion (2, 3, 4, 6);
+Transaccion tx_cuatro = new Transaccion (3, 4, 5, 15);
+Transaccion tx_cinco = new Transaccion (4, 7, 6, 64);
 
-Transaccion[] transacciones = new Transaccion[] {una, dos, tres, cuatro, cinco, seis, siete, ocho};
-            
+Transaccion[] transacciones = new Transaccion[] {tx_una, tx_dos, tx_tres, tx_cuatro, tx_cinco};           
 
-@Test
+    @Test
+    public void constructorMaxHeapTransacciones(){
+        MaxHeapTransacciones heaptx = new MaxHeapTransacciones(transacciones);
+        assertEquals(5,heaptx.tamaño());
+        assertEquals(4, heaptx.tamañoSinCreacion());
+        assertEquals(tx_cinco, heaptx.transaccionMayorValor());
+        assertEquals(89/4,heaptx.montoPromedio());
+    }
+  
+    @Test
+    public void desencolarMaximos(){
+        MaxHeapTransacciones heaptx = new MaxHeapTransacciones(transacciones);
 
-public void pruebasSimples(){
-    MaxHeapTransacciones prueba = new MaxHeapTransacciones();
-    prueba = prueba.testear(transacciones);
+        Transaccion[] heap_transacciones = new Transaccion[] {tx_cinco, tx_cuatro, tx_una, tx_tres, tx_dos}; // Armo una lista con las transacciones en orden de acuerdo a la de mayor monto
+        int tamaño = heap_transacciones.length;
 
-    assertEquals(22, prueba.montoPromedio());
-    assertEquals(new Transaccion(4, 7, 6, 64), prueba.transaccionMayorValor());
-}
+        for(int i=0; i<5; i++){
+            assertEquals(heap_transacciones[i], heaptx.transaccionMayorValor()); // Cheaquea que la transaccion sea la de mayor monto
+            heaptx.desencolar(); // Desencolo la transaccion de mayor valor del heaptx
+            tamaño --; 
+            assertEquals(tamaño, heaptx.tamaño()); // Chequeo que se haya disminuido el tamano
+        }
 
+        assertTrue(0 == heaptx.tamaño()); // El MaxHeapTransacciones quedo vacio
+    }
+    
+    @Test
+    public void copiaDeHeapTransaccionesAliasing(){
+        MaxHeapTransacciones tx_original = new MaxHeapTransacciones(transacciones);
 
+        // Hago una copia de las transacciones Originales
+        Transaccion[] copia = tx_original.copiaDeTransacciones();
 
+        // Modifico el primer elemento de la copia
+        copia[0] = new Transaccion(99, 99, 99, 999);
 
+        // Obtengo de nuevo la copia original
+        Transaccion[] originalInterno = tx_original.copiaDeTransacciones();
 
-
+        // Verifico que el original interno no e haya modificado
+        assertEquals(0, originalInterno[0].id()); // El id es 0 y no 4 porque copiaDeTransacciones() copia las transacciones en su orden original; no copia el heap.
+        assertEquals(10, originalInterno[0].monto());
+    }
 }
